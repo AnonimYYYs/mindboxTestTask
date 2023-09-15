c4a7a0955e86fc86ee06b6621b170ef6698b604a
Nikita <niknlo007@gmail.com>
2023-09-15 09:33:32 +0300

    Initial commit

---
+++ src/tasks/task1/squareCounter.py
@@ -0,0 +1,34 @@
+import math
+
+
+def countSquare(*args):
+    if len(args) == 1:
+        # Если передан один аргумент, считаем что это круг
+        return _countCircle(args[0])
+    elif len(args) == 3:
+        # Если передано три аргумента, считаем что это треугольник
+        return _countTriangle(*args)
+    else:
+        # Иначе вызываем ошибку
+        raise ValueError("Нет фигуры с заданным количеством параметров")
+
+
+
+def _countTriangle(a, b, c):
+    """
+    Вычисление по формуле Герона через полупериметр
+    """
+    s = (a + b + c) / 2
+
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+    return area
+
+
+def _countCircle(r):
+    """
+    Вычисление по стандартной формуле
+    """
+    area = math.pi * r ** 2
+
+    return area
---
+++ src/tasks/task1/squareUnittest.py
@@ -0,0 +1,28 @@
+import unittest
+from .squareCounter import _countCircle, _countTriangle, countSquare
+
+
+class TestGeometryFunctions(unittest.TestCase):
+
+    def test_circle_area(self):
+        # Проверяем вычисление площади круга
+        radius = 5.0
+        expected_area = 78.53981633974483
+        self.assertAlmostEqual(_countCircle(radius), expected_area, places=5)
+
+    def test_triangle_area(self):
+        # Проверяем вычисление площади треугольника
+        sides = (3, 4, 5)
+        expected_area = 6.0
+        self.assertAlmostEqual(_countTriangle(*sides), expected_area, places=5)
+
+    def test_no_arguments(self):
+        # Проверяем на ошибку без аргументов
+        with self.assertRaises(ValueError):
+            countSquare()
+
+    def test_six_arguments(self):
+        # Проверяем на ошибку с большим количеством аргументов
+        with self.assertRaises(ValueError):
+            countSquare(1, 2, 3, 4, 5, 6)
+
